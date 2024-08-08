59e706fddd81026636adf628b69acaa8b748d744
Krystian <krystian.kuzminski@kolektywkreatywny.pl>
2024-08-08 22:47:53 +0200

    Check light status

--- index.php
+++ index.php
@@ -11,32 +11,37 @@ $sunset = (new DateTime())->setTimestamp($sunInfo['sunset']);
 $sunriseEnd = (new DateTime())->setTimestamp($sunInfo['sunrise'] + 1800); // Sunrise lasts for 30 minutes
 $sunsetStart = (new DateTime())->setTimestamp($sunInfo['sunset'] - 1800); // Sunset starts 30 minutes before actual sunset
 
-if ($currentDateTime >= $sunrise && $currentDateTime <= $sunriseEnd) {
     $yee = new Yeelight("192.168.1.14", 55443);
-    $yee->set_power('off');
-    $yee->commit();
-    $yee->disconnect();
-} elseif ($currentDateTime >= $sunsetStart && $currentDateTime <= $sunset) {
-    $yee = new Yeelight("192.168.1.14", 55443);
-    $yee->set_power('on');
-    $yee->commit();
-    $yee->disconnect();
-} else {
-    echo "It is neither sunrise nor sunset in Lublin, Poland.";
-}
+    $status = $yee->get_prop("power")->commit();
+    print_r($status);
 
-$filename = 'log.txt';
-if (!file_exists($filename)) {
-    $file = fopen($filename, 'w');
-} else {
-    $file = fopen($filename, 'a');
-}
-
-if ($file) {
-    $current_time = date('d-m-Y H:i:s');
-    fwrite($file, $current_time . " - New log entry\n");
-    fclose($file);
-    echo "Log entry written successfully.";
-} else {
-    echo "Failed to open the file.";
-}
+    $yee->disconnect();
+//if ($currentDateTime >= $sunrise && $currentDateTime <= $sunriseEnd) {
+//    $yee = new Yeelight("192.168.1.14", 55443);
+//    $yee->set_power('off');
+//    $yee->commit();
+//    $yee->disconnect();
+//} elseif ($currentDateTime >= $sunsetStart && $currentDateTime <= $sunset) {
+//    $yee = new Yeelight("192.168.1.14", 55443);
+//    $yee->set_power('on');
+//    $yee->commit();
+//    $yee->disconnect();
+//} else {
+//    echo "It is neither sunrise nor sunset in Lublin, Poland.";
+//}
+//
+//$filename = 'log.txt';
+//if (!file_exists($filename)) {
+//    $file = fopen($filename, 'w');
+//} else {
+//    $file = fopen($filename, 'a');
+//}
+//
+//if ($file) {
+//    $current_time = date('d-m-Y H:i:s');
+//    fwrite($file, $current_time . " - New log entry\n");
+//    fclose($file);
+//    echo "Log entry written successfully.";
+//} else {
+//    echo "Failed to open the file.";
+//}
