9c3cf9d00133f17e9dd5bdbf5ffb3c730fa6cc19
Krystian <krystian.kuzminski@kolektywkreatywny.pl>
2024-08-08 22:20:15 +0200

    Check did is now sunrise or sunset

--- index.php
+++ index.php
@@ -1,9 +1,26 @@
 <?php
 require "Yeelight.class.php";
-$yee = new Yeelight("192.168.1.14", 55443);
 
-$yee->set_power('on');
+date_default_timezone_set('Europe/Warsaw');
+$currentDateTime = new DateTime();
+$lublinLatitude = 51.2465;
+$lublinLongitude = 22.5684;
+$sunInfo = date_sun_info($currentDateTime->getTimestamp(), $lublinLatitude, $lublinLongitude);
+$sunrise = (new DateTime())->setTimestamp($sunInfo['sunrise']);
+$sunset = (new DateTime())->setTimestamp($sunInfo['sunset']);
+$sunriseEnd = (new DateTime())->setTimestamp($sunInfo['sunrise'] + 1800); // Sunrise lasts for 30 minutes
+$sunsetStart = (new DateTime())->setTimestamp($sunInfo['sunset'] - 1800); // Sunset starts 30 minutes before actual sunset
 
-$yee->commit();
-
-$yee->disconnect();
+if ($currentDateTime >= $sunrise && $currentDateTime <= $sunriseEnd) {
+    $yee = new Yeelight("192.168.1.14", 55443);
+    $yee->set_power('off');
+    $yee->commit();
+    $yee->disconnect();
+} elseif ($currentDateTime >= $sunsetStart && $currentDateTime <= $sunset) {
+    $yee = new Yeelight("192.168.1.14", 55443);
+    $yee->set_power('on');
+    $yee->commit();
+    $yee->disconnect();
+} else {
+    echo "It is neither sunrise nor sunset in Lublin, Poland.";
+}
