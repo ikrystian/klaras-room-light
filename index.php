b0b1375470096d1482ce9122acdad1c496a61308
Krystian Kuźmiński <krystian.kuzminski@kolektywkreatywny.pl>
2024-08-09 21:21:02 +0200

    Revert some changes

--- index.php
+++ index.php
@@ -8,16 +8,15 @@ $lublinLongitude = 22.5684;
 $sunInfo = date_sun_info($currentDateTime->getTimestamp(), $lublinLatitude, $lublinLongitude);
 $sunrise = (new DateTime())->setTimestamp($sunInfo['sunrise']);
 $sunset = (new DateTime())->setTimestamp($sunInfo['sunset']);
-$sunriseEnd = (new DateTime())->setTimestamp($sunInfo['sunrise'] + 1800); // Sunrise lasts for 30 minutes
-$sunsetStart = (new DateTime())->setTimestamp($sunInfo['sunset'] - 1800); // Sunset starts 30 minutes before actual sunset
+$sunriseEnd = (new DateTime())->setTimestamp($sunInfo['sunrise'] + 120); // Sunrise lasts for 30 minutes
+$sunsetStart = (new DateTime())->setTimestamp($sunInfo['sunset'] - 120); // Sunset starts 30 minutes before actual sunset
 $yee = new Yeelight("192.168.1.14", 55443);
 
 if ($currentDateTime >= $sunrise && $currentDateTime <= $sunriseEnd) {
-    $yee->set_power('on')->set_bright(100);
+    $yee->set_power('on')->set_bright(100)->commit();
 
 } elseif ($currentDateTime >= $sunsetStart && $currentDateTime <= $sunset) {
-    $yee->set_power('off');
+    $yee->set_power('off')->commit();
 }
 
-$yee->commit();
 $yee->disconnect();
